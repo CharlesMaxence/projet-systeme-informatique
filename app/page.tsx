cfed2b998db7606dc2e21d985885cbba9146a858
charles <charles@sio.ndlp>
2025-11-21 11:39:41 +0100

    Ajout de l'affichage des réponses

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useEffect, useState } from 'react';
 import { supabase } from '../lib/supabaseClient';
@@ -9,9 +10,18 @@ export default function Home() {
   const [question, setQuestion] = useState<any>(null);
   useEffect(() => {
         async function fetchQuestion() {
-      const { data, error } = await supabase
+            const { data, error } = await supabase
         .from('question')
-        .select('*');
+        .select(`
+          id,
+          texte,
+          reponses:reponse (
+            id,
+            texte,
+            est_correcte
+          )
+        `)
+        .order('id', { ascending: true });
 
       if (error) console.error(error);
       else setQuestion(data[0]); // On stocke la première question dans l’état
@@ -19,6 +29,14 @@ export default function Home() {
 
     fetchQuestion();
   }, []);
+
+  function handleClick(reponse: any) {
+  if (reponse.est_correcte) {
+    alert("Bonne réponse !");
+  } else {
+    alert("Mauvaise réponse.");
+  }
+}
 
   return (
   <div>
@@ -36,6 +54,19 @@ export default function Home() {
     </CardHeader>
     <CardContent>
       <p>{question.texte}</p>
+
+{question.reponses.map((reponse: any) => (
+   <Button
+      key={reponse.id}
+      onClick={() => handleClick(reponse)}
+      className="w-full justify-start mt-4"
+      variant="outline"
+      >
+      {reponse.texte}
+   </Button>
+))}
+
+
     </CardContent>
   </Card>
 ) : (
